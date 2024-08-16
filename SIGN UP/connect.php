9c75e3dc61b3bfeddbaa53d222f1903b23e9785f
Prince-kay15 <109433125+Prince-kay15@users.noreply.github.com>
2024-08-16 12:11:30 +0100

    first commit

---
+++ SIGN UP/connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name= "online";
+
+$conn = mysqli_connect($host,$user,$pass,$db_name);
+
+// if (!$conn) {
+//     echo"Error connecting to database";
+// }
+// else {
+//     echo"Connection successful";
+// }
+
+?>
